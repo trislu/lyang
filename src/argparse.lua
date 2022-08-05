87b0a61ab766893c3bacb3afd5c92e251da0742e
Lu Kai <lukai@mail.ustc.edu.cn>
2022-08-05 17:00:50 +0800

    [update] argparse.lua + fix bugs + add corresponding test cases

--- src/argparse.lua
+++ src/argparse.lua
@@ -1,5 +1,3 @@
-#!/usr/bin/env lua
-
 --[[
 MIT License
 
@@ -24,26 +22,44 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ]]
 
-local parse_nargs = function(arg_list, start_pos, nargs_spec, option_name)
-    local argval_list = {}
-    if nargs_spec then
-    else
-        -- attempt to parse 1 value by default
-        if start_pos + 1 > #arg_list then
-            error('missing value for option "'..option_name..'"')
+local validate_option = function(option)
+    if 'table' == type(option) then
+        if #option == 0 then
+            error('invalid option : missing option name : short form or long form is needed')
+        elseif #option > 2 then
+            error('too much option names : short form or long form is needed')
+        else
+            local valid_acition = {
+                store = 1,
+                store_true = 2,
+                store_false = 3
+            }
+            if not (option.action and valid_acition[option.action]) then
+                error('invalid option : action must be one of "store"|"store_true"|"store_false"')
+            end
+            local valid_nargs = {
+                ['*']=1,
+                ['+']=2,
+                ['?']=3
+            }
+            if option.nargs and not (('number' == type(option.nargs)) or valid_nargs[option.nargs]) then
+                error('invalid option : nargs must be one of number|"?"|"*"|"+"')
+            end
         end
+    else
+        error('invalid option : must be a table')
     end
 end
 
 local m = {
---[[light weight python-like ArgumentParser]]
-    create = function ()
+    --[[light weight python-like ArgumentParser]]
+    create = function()
         local option_list = {}
         local option_search_table = {}
         local parser = {
-            add_argument = function(a)
+            add_argument = function(o)
                 --[[
-                    the input 'arg' be like:
+                    the input option 'o' must be table like:
                     {
                         [1] & [2]: short/long form of the option name, e.g. '-h' , e.g. '--help'
                         action : The basic type of action to be taken when this argument is encountered at the command line.
@@ -52,57 +68,95 @@ local m = {
                         help: A brief description of what the argument does.
                     }
                 ]]
-                option_list[#option_list+1] = a
+                validate_option(o)
+                option_list[#option_list + 1] = o
                 -- iterate [1] [2] ... to store argument with its option name
-                for _, option_name in ipairs(a) do
+                for _, option_name in ipairs(o) do
+                    if not string.match(option_name, "^(%-%-?)[^%-]+") then
+                        error('invalid option name: "'..option_name..'", must start with "-" or "--"')
+                    end
                     -- create option table for option-key-searching
-                    option_search_table[option_name] = a
+                    option_search_table[option_name] = o
                 end
             end,
             print_usage = function()
-                print('Usage: lyang [options] file...')
-                print('Available options:')
+                print("Usage: lyang [file] [options]")
+                print("Available options:")
                 for _, option in ipairs(option_list) do
-                    local names = option[1]..(option[2] and (', '..option[2]) or '')
-                    print('  '..names..'\t'..option.help)
+                    local names = option[1] .. (option[2] and (", " .. option[2]) or "")
+                    print("  " .. names .. "\t" .. option.help)
                 end
             end,
-            parse_args = function()
+            parse_args = function(arglist)
                 local result = {}
-                local arglist = arg
                 local index = 1
-                while index < #arglist do
-                    local _arg = option_list[index]
-                    -- search if '_arg' is an option
+                while index <= #arglist do
+                    local _arg = arglist[index]
+                    -- search if '_arg' is a valid option name
                     local option = option_search_table[_arg]
                     if option then
                         -- option does exists
                         local action = option.action
-                        if action == 'store_true' then
+                        if action == "store_true" then
                             -- store true to the result with option.dest as key
                             result[option.dest] = true
-                        elseif action == 'store_false' then
+                        elseif action == "store_false" then
                             -- store false to the result with option.dest as key
                             result[option.dest] = false
-                        elseif action == 'store' then
+                        elseif action == "store" or action == nil then
+                            -- parse nargs
+                            local nargs = {}
+                            -- repeat parse next arguments
+                            while (index+1 <= #arglist) and (not string.match(arglist[index+1], "^(%-%-?)[^%-]+")) do
+                                nargs[#nargs + 1] = arglist[index+1]
+                                index = index + 1
+                            end
+                            -- check if nargs is valid
+                            if option.nargs then
+                                if 'number' == type(option.nargs) then
+                                    if #nargs ~= option.nargs then
+                                        error('option "'.._arg..'" expect '..tostring(option.nargs)..' arguments while '..tostring(nargs)..' given')
+                                    end
+                                elseif '?' == option.nargs then
+                                    if #nargs > 1 then
+                                        error('option "'.._arg..'" expect 0 or 1 argument while '..tostring(nargs)..' given')
+                                    end
+                                elseif '+' == option.nargs then
+                                    if #nargs < 1 then
+                                        error('option "'.._arg..'" expect 1 or more argument while 0 given')
+                                    end
+                                elseif '*' == option.nargs then
+                                    -- good
+                                else
+                                    error('option "'.._arg..'" invalid nargs '..option.nargs)
+                                end
+                            else
+                                error('option "'.._arg..'" claims store without nargs')
+                            end
+                            result[option.dest] = nargs
                         end
                     else
-                        if string.match(_arg, '^(%-%-?)[^%-]+') then
-                            -- '_arg' seem to be an option
-                            error('invalid option: '.._arg)
+                        if string.match(_arg, "^(%-%-?)[^%-]+") then
+                            -- '_arg' seem to fit the option format
+                            error("invalid option: " .. _arg)
                         end
                         -- push '_arg' as positional arguments
                         result[#result + 1] = _arg
                     end
+                    -- move index
+                    index = index + 1
                 end
+                -- return parsed result
+                return result
             end
         }
         -- add default options
-        parser.add_argument{
-            '-h', '--help',
-            action='store_true',
-            dest='help',
-            help='Display this information'
+        parser.add_argument {
+            "-h",
+            "--help",
+            action = "store_true",
+            dest = "help",
+            help = "Display this information"
         }
         return parser
     end
--- test/test_argparse.lua
+++ test/test_argparse.lua
@@ -29,12 +29,100 @@ function test_print_usage()
     _sandbox.print = function(params)
         -- sandbox print
         if params then
-            genv.print('[sandbox.print]', params)
+            --genv.print('[sandbox.print]', params)
         end
     end
     genv.setfenv(p.print_usage, _sandbox)
     local status, err = genv.pcall(p.print_usage)
     assert_true( status, err )
+end
+
+function test_add_arguments()
+    local p = argparse.create()
+    -- nil
+    assert_false( genv.pcall(p.add_argument, nil) )
+    -- string
+    assert_false( genv.pcall(p.add_argument, 'foo') )
+    -- number
+    assert_false( genv.pcall(p.add_argument, 123) )
+    -- boolean
+    assert_false( genv.pcall(p.add_argument, true) )
+    assert_false( genv.pcall(p.add_argument, false) )
+    -- invalid table : name not given
+    local invalid_option = {
+        help='just for test'
+    }
+    -- option name not given
+    assert_false( genv.pcall(p.add_argument, invalid_option) )
+    -- too much option names
+    invalid_option[#invalid_option+1] = '-t'
+    invalid_option[#invalid_option+1] = '--test'
+    invalid_option[#invalid_option+1] = 'bad'
+    assert_false( genv.pcall(p.add_argument, invalid_option) )
+    -- action not specified
+    invalid_option[3] = nil
+    invalid_option.action = nil
+    assert_false( genv.pcall(p.add_argument, invalid_option) )
+    -- unknown action
+    invalid_option.action = 'unknown'
+    assert_false( genv.pcall(p.add_argument, invalid_option) )
+    -- unknown nargs
+    invalid_option.action = 'store'
+    invalid_option.nargs = 'zzz'
+    assert_false( genv.pcall(p.add_argument, invalid_option) )
+    -- invalid option name
+    invalid_option.nargs = nil
+    invalid_option[1] = 'tt'
+    assert_false( genv.pcall(p.add_argument, invalid_option) )
+    -- fix every thing, now it becomes a valid option
+    invalid_option[1] = '-t'
+    assert_true( genv.pcall(p.add_argument, invalid_option) )
+end
+
+function test_parse_args()
+    local p = argparse.create()
+    -- prepare test option
+    local test_option = {
+        "-t",
+        "--test",
+        action = "store_true",
+        dest = "test",
+        help = "Display this information"
+    }
+    p.add_argument(test_option)
+    -- test positional argument
+    local status, ret = genv.pcall(p.parse_args, {"test.lua"})
+    assert_true( status )
+    assert_equal(#ret, 1)
+    assert_equal(ret[1], 'test.lua')
+    -- test short form
+    status, ret = genv.pcall(p.parse_args, {"-t"})
+    assert_true( status )
+    -- test long form
+    status, ret = genv.pcall(p.parse_args, {"--test"})
+    assert_true( status )
+    -- test action
+    status, ret = genv.pcall(p.parse_args, {"--test"})
+    assert_true( status )
+    assert_not_nil(ret.test)
+    assert_boolean(ret.test)
+    -- test nargs
+    local nargs_option = {
+        "-n",
+        "--nargs",
+        action = "store",
+        nargs = 2,
+        dest = "result",
+        help = "Display this information"
+    }
+    p.add_argument(nargs_option)
+    status, ret = genv.pcall(p.parse_args, {"--nargs", "a", "b"})
+    assert_true( status )
+    assert_not_nil(ret.result)
+    assert_table(ret.result)
+    assert_equal(2, #ret.result)
+    assert_equal('a', ret.result[1])
+    assert_equal('b', ret.result[2])
 end
 
 lunit.main(...)

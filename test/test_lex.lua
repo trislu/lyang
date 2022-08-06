a19d97793553874ba2aee4594bc2b607c9832a62
Lu Kai <lukai@mail.ustc.edu.cn>
2022-08-06 19:27:07 +0800

    [unittest] add new cases for test_lex.lua + single character token + line comment + block comment

--- test/test_lex.lua
+++ test/test_lex.lua
@@ -87,4 +87,85 @@ function test_double_quoted_string()
     assert_equal('world', b.sub(tk2.start, tk2.start + tk2.length))
 end
 
+function test_single_character()
+    local b = buffer()
+    local str = 'hello "world";'
+    b.loadstring(str)
+    local lexer = lex(b)
+    local tokens = {}
+    while true do
+        local tk = lexer.next_token()
+        if tk then
+            tokens[#tokens+1] = tk
+        else
+            break
+        end
+    end
+    local tk1 = tokens[3]
+    assert_equal(token.CHAR, tk1.type)
+    assert_equal(';', b.sub(tk1.start, tk1.start + tk1.length))
+end
+
+function test_line_comment()
+    local b = buffer()
+    local str = [[
+            hello
+            // skip this line
+            "world";
+        ]]
+    b.loadstring(str)
+    local lexer = lex(b)
+    local tokens = {}
+    while true do
+        local tk = lexer.next_token()
+        if tk then
+            tokens[#tokens+1] = tk
+        else
+            break
+        end
+    end
+    assert_equal(3, #tokens)
+    local tk1 = tokens[1]
+    assert_equal(token.UQSTR, tk1.type)
+    assert_equal('hello', b.sub(tk1.start, tk1.start + tk1.length))
+    local tk2 = tokens[2]
+    assert_equal(token.DQSTR, tk2.type)
+    assert_equal('world', b.sub(tk2.start, tk2.start + tk2.length))
+    local tk3 = tokens[3]
+    assert_equal(token.CHAR, tk3.type)
+    assert_equal(';', b.sub(tk3.start, tk3.start + tk3.length))
+end
+
+function test_block_comment()
+    local b = buffer()
+    local str = [[
+            hello
+            /* 
+                skip this block
+            */
+            "world";
+        ]]
+    b.loadstring(str)
+    local lexer = lex(b)
+    local tokens = {}
+    while true do
+        local tk = lexer.next_token()
+        if tk then
+            tokens[#tokens+1] = tk
+        else
+            break
+        end
+    end
+    assert_equal(3, #tokens)
+    local tk1 = tokens[1]
+    assert_equal(token.UQSTR, tk1.type)
+    assert_equal('hello', b.sub(tk1.start, tk1.start + tk1.length))
+    local tk2 = tokens[2]
+    assert_equal(token.DQSTR, tk2.type)
+    assert_equal('world', b.sub(tk2.start, tk2.start + tk2.length))
+    local tk3 = tokens[3]
+    assert_equal(token.CHAR, tk3.type)
+    assert_equal(';', b.sub(tk3.start, tk3.start + tk3.length))
+end
+
 lunit.main(...)

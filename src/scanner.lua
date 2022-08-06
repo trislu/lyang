a6dc537ab40a61478c455b947ddda0b554b1b969
Lu Kai <lukai@mail.ustc.edu.cn>
2022-08-06 10:53:48 +0800

    [create] scanner.lua && test/test_scanner.lua

---
+++ src/scanner.lua
@@ -0,0 +1,81 @@
+--[[
+MIT License
+
+Copyright (c) 2022 Lu Kai
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
+
+local token = require('token')
+
+return function(buf)
+    local cur = nil
+    local con = nil
+    local line = 1
+    local column = 0
+    local buffer = buf
+    local s = {
+        next = function()
+            if nil == cur then
+                cur = 0
+                return buffer.at(0)
+            elseif cur >= buffer.len() then
+                return nil
+            end
+            cur = cur + 1
+            local n = buffer.at(cur)
+            if '\r' == n then
+                -- windows?
+                cur = cur + 1
+                n = buffer.at(cur)
+            end
+            if '\n' == n then
+                column = 0
+                line = line + 1
+            end
+            return n
+        end,
+        peek = function ()
+            if nil == cur then
+                return buffer.at(0)
+            elseif cur >= buffer.len() then
+                return nil
+            end
+            return buffer.at(cur + 1)
+        end,
+        peek2 = function ()
+            if buffer.len() < 2 then
+                return nil
+            elseif nil == cur then
+                return buffer.sub(0, 1)
+            elseif cur + 2 >= buffer.len() then
+                return nil
+            end
+            return buffer.sub(cur + 1, cur + 2)
+        end,
+        consume = function()
+            con = cur
+        end,
+        make_token = function(t)
+            -- make
+            return token.create(t, con, cur-con, line, column)
+        end
+    }
+    return s
+end
---
+++ test/test_scanner.lua
@@ -0,0 +1,84 @@
+-- make sure test cases can source 'lunit'
+package.path = package.path .. ';../3rd/lunit/?.lua'
+-- make sure test cases can source 'src'
+package.path = package.path .. ';../src/?.lua'
+
+-- cache global env firstly
+local genv = {
+    -- add needed global functions here
+    getfenv = getfenv,
+    pcall = pcall,
+    print = print,
+    setfenv = setfenv,
+}
+
+local buffer = require('buffer')
+local scanner = require('scanner')
+local token = require('token')
+
+-- global env will be broke by lunit
+require('lunit')
+module( "test_scanner", lunit.testcase)
+
+function test_create()
+    assert_function(scanner)
+    local s = scanner(buffer())
+    assert_table(s)
+end
+
+function test_next()
+    local b = buffer()
+    b.loadstring("hello world")
+    local s = scanner(b)
+    for i = 1, b.len() do
+        -- statements
+        local ch = s.next()
+        assert_equal(ch, b.at(i-1))
+    end
+    assert_nil(s.next())
+end
+
+function test_peek()
+    local b = buffer()
+    b.loadstring("hello world")
+    local s = scanner(b)
+    assert_equal('h', s.peek())
+end
+
+function test_peek2()
+    local b = buffer()
+    b.loadstring("hello world")
+    local s1 = scanner(b)
+    assert_equal('he', s1.peek2())
+    b.loadstring("d")
+    local s2 = scanner(b)
+    assert_equal(1, b.len())
+    --assert_nil(s2.peek2())
+end
+
+function test_make_token()
+    local b = buffer()
+    b.loadstring("hello world")
+    local s = scanner(b)
+    local ch = s.next() -- h
+    s.consume() -- point to 'h'
+    local tk1 = s.make_token(token.CHAR)
+    assert_equal('h', b.sub(tk1.start, tk1.start + tk1.length))
+    s.next() -- e
+    s.next() -- l
+    s.next() -- l
+    s.next() -- o
+    local tk2 = s.make_token(token.UQSTR)
+    assert_equal('hello', b.sub(tk2.start, tk2.start + tk2.length))
+    s.next() -- skip ws
+    s.next() -- w
+    s.consume() -- point to 'w'
+    s.next() -- o
+    s.next() -- r
+    s.next() -- l
+    s.next() -- d
+    local tk3 = s.make_token(token.UQSTR)
+    assert_equal('world', b.sub(tk3.start, tk3.start + tk3.length))
+end
+
+lunit.main(...)
